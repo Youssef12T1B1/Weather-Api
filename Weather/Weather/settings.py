2a6a1121f8a677007ef366a7eaf0c5ace33bd752
Youssef12T1B1 <yousseattabi1998@gmail.com>
2022-02-06 14:30:24 -0800

    check

--- Weather/Weather/settings.py
+++ Weather/Weather/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-br6^u()1)5!s+6yud84v__=1cv2qv6tsr2r5&+y7s9s0oa7hjo'
+
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
